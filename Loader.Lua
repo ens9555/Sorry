f45b6e6c4ba1349e03f020cda15c9d2fa5735a0b
ens9555 <50376570+ens9555@users.noreply.github.com>
2026-09-23 02:23:19 +0700

    Update Loader.Lua

--- Loader.Lua
+++ Loader.Lua
@@ -15,7 +15,7 @@ local SAVE_FILE = "MD_MasensDevHub_KeySave.json"
 local KEY_EXPIRE_TIME = 12 * 3600 -- 12 Jam dalam detik (43.200 detik)
 
 -- LINK GET KEY
-local GET_KEY_LINK = "https://link-hub.net/9347872/OeHjUSdeYOef" 
+local GET_KEY_LINK = "https://link-center.net/9347872/hhoRnAmsCNAH" 
 
 -- Helper Simpan Data Key + Timestamp (Hanya dipanggil saat PERTAMA KALI LOGIN)
 local function createNewSaveData(key)
